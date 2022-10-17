11b0b9900ef47d22180a00178b5d51b9584d05ab
Austinedmar <domingosmussumar@gmail.com>
2022-10-18 00:39:52 +0100

    component header e footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,25 @@
+<template>
+ <footer>
+    <p>Ranek, Alguns direitos autorais reservados</p>
+ </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer-compo"
+}
+</script>
+
+<style scoped>
+
+    footer{
+        background: #87f;
+        padding: 25px 0;
+        text-align: center;
+        font-size: 1.2rem;
+        color: aliceblue;
+        position: relative;
+        top: 75px;
+    }
+
+</style>
